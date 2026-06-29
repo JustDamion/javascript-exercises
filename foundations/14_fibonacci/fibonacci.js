e8642f4fa8f2d7820932e3fc60d4087edf0eab30
Damion Shirkey <shirkeydamion@gmail.com>
2026-06-29 13:51:25 -0500

    Complete foundations 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(place) {
+    if (place < 0) return "OOPS";
 
+    let fib = [0, 1]
+    for (let i = 2; i <= place; i++) {
+        let fibNumber = fib[i - 2] + fib[i - 1];
+        fib.push(fibNumber);
+    }
+
+    return fib[place];
 };
 
 // Do not edit below this line
