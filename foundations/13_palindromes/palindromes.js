2e081569163f7f20849884e7bbdb223cd55d4138
Damion Shirkey <shirkeydamion@gmail.com>
2026-06-29 13:38:09 -0500

    Complete foundations 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = word
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join("");
+
+    const reverse = cleanedString.split("").reverse().join("");
+    return cleanedString === reverse;
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
