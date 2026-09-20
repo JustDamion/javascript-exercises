bd492f527575d5ac3074810ad2d53c0bc09a50fc
Damion Shirkey <shirkeydamion@gmail.com>
2026-09-20 18:15:11 -0500

    Complete computer_science recursion 01

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (number) {
+  if (number === 0) {
+    return 1;
+  }
+  if (number < 0 || !Number.isInteger(number)) {
+    return;
+  }
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
