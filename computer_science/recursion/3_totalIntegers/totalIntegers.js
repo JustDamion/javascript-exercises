6751aad5dc35d7d9be89cb8471f1d3beacb4ec9a
Damion Shirkey <shirkeydamion@gmail.com>
2026-09-20 21:51:15 -0500

    Complete computer_science recursion 03

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,22 @@
-const totalIntegers = function() {
-  
+const isObject = (value) => typeof value === "object" && value !== null;
+
+const totalIntegers = function (object) {
+  let total = 0;
+
+  if (!isObject(object)) return;
+
+  const values = Object.values(object);
+
+  values.map((value) => {
+    if (Number.isInteger(value)) {
+      total++;
+    } else if (isObject(value)) {
+      total += totalIntegers(value);
+    }
+  });
+
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
