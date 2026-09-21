f5a7ec59efd806e71765e4a80d261b23fbfe24cd
Damion Shirkey <shirkeydamion@gmail.com>
2026-09-21 00:01:23 -0500

    Complete computer_science recursion 04

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,19 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (arr.length === 0) return [[]];
+
+  const perms = permutations(arr.slice(1));
+  const result = [];
+
+  for (const p of perms) {
+    for (let i = 0; i <= p.length; i++) {
+      const copy = p.slice();
+      copy.splice(i, 0, arr[0]);
+      result.push(copy);
+    }
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
