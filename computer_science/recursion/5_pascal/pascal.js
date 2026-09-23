d3fc1c5ae2d49582287441b3469cfefa0d153328
Damion Shirkey <shirkeydamion@gmail.com>
2026-09-22 20:34:52 -0500

    Complete computer_science recursion 05

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,15 @@
-const pascal = function() {
-  
+const pascal = function (row) {
+  const arr = [1];
+  if (row === 1) return arr;
+
+  const previous = pascal(row - 1);
+  previous.forEach((number, i) => {
+    const next = previous[i + 1] ?? 0;
+    arr.push(number + next);
+  });
+
+  return arr;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
